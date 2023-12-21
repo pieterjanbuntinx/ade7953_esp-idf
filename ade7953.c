f9e346bcfe35be22a9c97a4b3e940a6c1aadd75b
pjbuntinx2 <pieterjan@elecgator.com>
2023-12-21 17:35:35 +0100

    added most functions

---
+++ ade7953.c
@@ -0,0 +1,279 @@
+#include "ade7953.h"
+#include <driver/i2c.h>
+#include <esp_err.h>
+#include <string.h>
+#include <nvs_flash.h>
+#include <esp_timer.h>
+
+const uint16_t Ade7953CalibRegs[2][ADE7953_CALIBREGS] = {{ADE7953_AVGAIN, ADE7953_AIGAIN, ADE7953_AWGAIN, ADE7953_AVAGAIN, ADE7953_AVARGAIN, ADE7943_PHCALA},
+                                                         {ADE7953_BVGAIN, ADE7953_BIGAIN, ADE7953_BWGAIN, ADE7953_BVAGAIN, ADE7953_BVARGAIN, ADE7943_PHCALB}};
+
+const float ADE7953_LSB_PER_WATTSECOND = 2.5;
+const float ADE7953_POWER_CORRECTION   = 23.41494; // See https://github.com/arendst/Tasmota/pull/16941
+
+static void ade7953_task(void* arg);
+
+static size_t ade7953_getRegSize(uint16_t reg) {
+    size_t size = 0;
+    switch ((reg >> 8) & 0x0F) {
+    case 0x03: // 32-bit
+        size++;
+    case 0x02: // 24-bit
+        size++;
+    case 0x01: // 16-bit
+        size++;
+    case 0x00: // 8-bit
+    case 0x07:
+    case 0x08:
+        size++;
+    }
+    return size;
+}
+
+static esp_err_t readData(ade7953_t* ade7953, uint16_t reg, size_t len, void* data, TickType_t ticks_to_wait) {
+    uint16_t  reg_temp = ((reg >> 8) | (reg << 8)) & 0xFFFF;
+    esp_err_t err      = i2c_master_write_read_device(ade7953->i2c_port_number, ade7953->addr, (uint8_t*)&reg_temp, 2, data, len, ticks_to_wait);
+    vTaskDelay(pdMS_TO_TICKS(1));
+    return err;
+}
+
+int32_t ade7953_readReg(ade7953_t* ade7953, uint16_t reg, TickType_t ticks_to_wait) {
+    int32_t data = 0;
+    size_t  size = ade7953_getRegSize(reg);
+    readData(ade7953, reg, size, &data, ticks_to_wait);
+
+    int32_t ret = 0;
+    for (int i = 0; i < size; i++) {
+        ret = ret << 8 | ((uint8_t*)&data)[i];
+    }
+    return ret;
+}
+
+esp_err_t writeData(ade7953_t* ade7953, uint16_t reg, size_t len, void* data, TickType_t ticks_to_wait) {
+    uint16_t reg_temp       = ((reg >> 8) | (reg << 8)) & 0xFFFF;
+    uint8_t* data_with_addr = malloc(len + 2);
+    memcpy(data_with_addr, &reg_temp, 2);
+
+    // MSB first
+    for (int i = 0; i < len; i++) {
+        uint8_t* ptr          = data + (len - 1) - i;
+        data_with_addr[2 + i] = *ptr;
+    }
+
+    // printf("reg: 0x%04x, len: %i, data: ", reg, len);
+    // for (int i = 0; i < len; i++) {
+    //     printf("%02x", ((uint8_t*)data)[i]);
+    // }
+    // printf(" data_with_addr: ");
+    // for (int i = 0; i < len + 2; i++) {
+    //     printf("%02x", ((uint8_t*)data_with_addr)[i]);
+    // }
+    // printf("\n");
+
+    esp_err_t err = i2c_master_write_to_device(ade7953->i2c_port_number, ade7953->addr, data_with_addr, len + 2, ticks_to_wait);
+    vTaskDelay(pdMS_TO_TICKS(1));
+    free(data_with_addr);
+    return err;
+}
+
+void ade7953_writeReg(ade7953_t* ade7953, uint16_t reg, int32_t data, TickType_t ticks_to_wait) {
+    size_t size = ade7953_getRegSize(reg);
+    writeData(ade7953, reg, size, &data, ticks_to_wait);
+
+    // int32_t d = ade7953_readReg(ade7953, reg, pdMS_TO_TICKS(100));
+    // printf("read: %04x, data: ", reg);
+    // for (int i = 0; i < size; i++) {
+    //     printf("%02x", ((uint8_t*)&d)[i]);
+    // }
+    // printf("\n");
+}
+
+void ade7953_setCalibration(ade7953_t* ade7953, uint32_t regset, uint32_t calibset) {
+    for (uint32_t i = 0; i < ADE7953_CALIBREGS; i++) {
+        vTaskDelay(pdMS_TO_TICKS(100));
+        int32_t value = ade7953->calib_data[calibset][i];
+        if (ADE7943_CAL_PHCAL == i) {
+            //      if (ADE7953_PHCAL_DEFAULT == value) { continue; }  // ADE7953
+            //      reset does NOT always reset all registers
+            if (value < 0) {
+                value = abs(value) + 0x200; // Add sign magnitude
+            }
+        }
+        //    if (ADE7953_GAIN_DEFAULT == value) { continue; }  // ADE7953 reset
+        //    does NOT always reset all registers
+        ade7953_writeReg(ade7953, Ade7953CalibRegs[regset][i], value, pdMS_TO_TICKS(100));
+    }
+}
+
+void ade7953_setDefaultsForShellyDevice(ade7953_t* ade7953, ShellyModels_t model) {
+    for (uint32_t channel = 0; channel < ADE7953_MAX_CHANNEL; channel++) {
+        for (uint32_t i = 0; i < ADE7953_CALIBREGS; i++) {
+            if (ADE7943_CAL_PHCAL == i) {
+                ade7953->calib_data[channel][i] = (ADE7953_SHELLY_EM == model) ? ADE7953_PHCAL_DEFAULT_CT : ADE7953_PHCAL_DEFAULT;
+            } else if (ADE7953_CAL_VGAIN == i) {
+                ade7953->calib_data[channel][i] = (int32_t)((float)ADE7953_GAIN_DEFAULT * 0.378129);
+            } else if (ADE7953_CAL_IGAIN == i) {
+                ade7953->calib_data[channel][i] = (int32_t)((float)ADE7953_GAIN_DEFAULT * 0.935529);
+            } else if (ADE7953_CAL_WGAIN == i || ADE7953_CAL_VAGAIN == i || ADE7953_CAL_VARGAIN == i) {
+                ade7953->calib_data[channel][i] = (int32_t)((float)ADE7953_GAIN_DEFAULT * 0.943565);
+            } else {
+                ade7953->calib_data[channel][i] = ADE7953_GAIN_DEFAULT;
+            }
+        }
+    }
+}
+
+void ade7953_retrieveCalibrationFromNvs(ade7953_t* ade7953) {
+    nvs_handle_t nvs_handle;
+    esp_err_t    err = nvs_open("nvs", NVS_READWRITE, &nvs_handle);
+
+    if (err == ESP_OK) {
+        size_t required_size = 0;
+
+        char nvs_key[32] = {0};
+        sprintf(nvs_key, "ade7953_cal_%c", ade7953->chip_num);
+
+        err = nvs_get_blob(nvs_handle, nvs_key, NULL, &required_size);
+        if ((err == ESP_OK) && (required_size == ADE7953_MAX_CHANNEL * ADE7953_CALIBREGS)) {
+            int32_t calib_data[ADE7953_MAX_CHANNEL][ADE7953_CALIBREGS];
+            nvs_get_blob(nvs_handle, nvs_key, &calib_data, &required_size);
+            memcpy(&ade7953->calib_data, &calib_data, required_size);
+        }
+    }
+}
+
+void ade7953_saveCalibrationFromNvs(ade7953_t* ade7953) {
+    nvs_handle_t nvs_handle;
+    esp_err_t    err = nvs_open("nvs", NVS_READWRITE, &nvs_handle);
+
+    if (err == ESP_OK) {
+        char nvs_key[32] = {0};
+        sprintf(nvs_key, "ade7953_cal_%c", ade7953->chip_num);
+        nvs_set_blob(nvs_handle, nvs_key, &ade7953->calib_data, ADE7953_MAX_CHANNEL * ADE7953_CALIBREGS);
+    }
+}
+
+void ade7953_init(ade7953_t* ade7953, ShellyModels_t model) {
+    ade7953_setDefaultsForShellyDevice(ade7953, model);
+
+    ade7953_writeReg(ade7953, ADE7953_CONFIG, 0x0004, pdMS_TO_TICKS(100));                        // Locking the communication interface
+                                                                                                  // (Clear bit COMM_LOCK), Enable HPF
+    ade7953_writeReg(ade7953, 0x0FE, 0x00AD, pdMS_TO_TICKS(100));                                 // Unlock register 0x120
+    ade7953_writeReg(ade7953, ADE7953_RESERVED_0X120, 0x0030, pdMS_TO_TICKS(100));                // Configure optimum setting
+    ade7953_writeReg(ade7953, ADE7953_DISNOLOAD, 0x07, pdMS_TO_TICKS(100));                       // Disable no load detection, required before setting thresholds
+    ade7953_writeReg(ade7953, ADE7953_AP_NOLOAD, ADE7953_NO_LOAD_THRESHOLD, pdMS_TO_TICKS(100));  // Set no load treshold for active power
+    ade7953_writeReg(ade7953, ADE7953_VAR_NOLOAD, ADE7953_NO_LOAD_THRESHOLD, pdMS_TO_TICKS(100)); // Set no load treshold for reactive power
+    ade7953_writeReg(ade7953, ADE7953_DISNOLOAD, 0x00, pdMS_TO_TICKS(100));                       // Enable no load detection
+
+    // ade7953_retrieveCalibrationFromNvs(ade7953);
+    ade7953_setCalibrationForShellyDevice(ade7953, model);
+
+    char taskName[32];
+    sprintf(taskName, "ade7953_task%c", ade7953->chip_num);
+    xTaskCreate(ade7953_task, taskName, 2048, ade7953, configMAX_PRIORITIES - 3, NULL);
+}
+
+void ade7953_setCalibrationForShellyDevice(ade7953_t* ade7953, ShellyModels_t model) {
+    ade7953_setCalibration(ade7953, 0, 0); // First ADE7953 A registers set with calibration set 0
+    switch (model) {
+    case ADE7953_SHELLY_25:
+    case ADE7953_SHELLY_EM:
+    case ADE7953_SHELLY_PLUS_2PM:
+    // case ADE7953_SHELLY_PRO_1PM:          // Uses defaults for B registers
+    case ADE7953_SHELLY_PRO_4PM:
+        ade7953_setCalibration(ade7953, 1, 1); // First ADE7953 B registers set with calibration set 1
+    default:
+        break;
+    }
+
+    int32_t regs[ADE7953_CALIBREGS];
+    for (uint32_t channel = 0; channel < 2; channel++) {
+        for (uint32_t i = 0; i < ADE7953_CALIBREGS; i++) {
+            regs[i] = ade7953_readReg(ade7953, Ade7953CalibRegs[channel][i], pdMS_TO_TICKS(100));
+            if (ADE7943_CAL_PHCAL == i) {
+                if (regs[i] >= 0x0200) {
+                    regs[i] &= 0x01FF; // Clear sign magnitude
+                    regs[i] *= -1;     // Make negative
+                }
+            }
+        }
+        printf("ADE: CalibRegs%c V %d, I %d, W %d, VA %d, VAr %d, Ph %d\n", 'A' + channel, regs[0], regs[1], regs[2], regs[3], regs[4], regs[5]);
+    }
+}
+
+static void ade7953_task(void* arg) {
+    ade7953_t* ade7953 = (ade7953_t*)arg;
+
+    nvs_handle_t nvs_handle;
+    esp_err_t    err = nvs_open("nvs", NVS_READWRITE, &nvs_handle);
+
+    if (ade7953->saveEnergiesToNvs) {
+        if (err == ESP_OK) {
+            size_t required_size = 0;
+
+            char nvs_key[32] = {0};
+            sprintf(nvs_key, "ade7953_ena_%c", ade7953->chip_num);
+
+            int64_t value = 0;
+            if (nvs_get_i64(nvs_handle, nvs_key, &value) == ESP_OK) {
+                ade7953->data[0].activeEnergy = (double)value / 1000.0;
+            }
+            value = 0;
+            sprintf(nvs_key, "ade7953_enb_%c", ade7953->chip_num);
+            if (nvs_get_i64(nvs_handle, nvs_key, &value) == ESP_OK) {
+                ade7953->data[1].activeEnergy = (double)value / 1000.0;
+            }
+        }
+    }
+
+    int64_t  lastRetrievalTime = 0;
+    int64_t  currentTime       = 0;
+    uint32_t iRmsA, iRmsB, vRms;
+    int32_t  accEnergyA, accEnergyB;
+
+    int64_t time = 0;
+
+    TickType_t lastEnergySavedTime = 0;
+    while (1) {
+        vRms  = ade7953_readReg(ade7953, ADE7953_VRMS, pdMS_TO_TICKS(100));
+        iRmsA = ade7953_readReg(ade7953, ADE7953_IRMSA, pdMS_TO_TICKS(100));
+        iRmsB = ade7953_readReg(ade7953, ADE7953_IRMSB, pdMS_TO_TICKS(100));
+
+        currentTime = esp_timer_get_time();
+        accEnergyA  = ade7953_readReg(ade7953, ADE7953_AENERGYA, pdMS_TO_TICKS(100));
+        accEnergyB  = ade7953_readReg(ade7953, ADE7953_AENERGYB, pdMS_TO_TICKS(100));
+
+        time              = currentTime - lastRetrievalTime;
+        lastRetrievalTime = currentTime;
+
+        ade7953->data[0].voltage     = (float)vRms / 10000.0;
+        ade7953->data[0].current     = (float)iRmsA / 100000.0;
+        ade7953->data[0].activePower = (float)abs(accEnergyA) / ADE7953_LSB_PER_WATTSECOND / (time / 1000 / 1000);
+        ade7953->data[0].activeEnergy += (float)abs(accEnergyA) / ADE7953_LSB_PER_WATTSECOND / 3600 / 1000;
+
+        ade7953->data[1].voltage     = (float)vRms / 10000.0;
+        ade7953->data[1].current     = (float)iRmsB / 100000.0;
+        ade7953->data[1].activePower = (float)abs(accEnergyB) / ADE7953_LSB_PER_WATTSECOND / 1 / (time / 1000 / 1000);
+        ade7953->data[1].activeEnergy += (float)abs(accEnergyB) / ADE7953_LSB_PER_WATTSECOND / 3600 / 1000;
+
+        if (ade7953->saveEnergiesToNvs) {
+            if (pdTICKS_TO_MS(xTaskGetTickCount() - lastEnergySavedTime) > 60 * 1000) {
+                nvs_handle_t nvs_handle;
+                esp_err_t    err = nvs_open("nvs", NVS_READWRITE, &nvs_handle);
+
+                if (err == ESP_OK) {
+                    char nvs_key[32] = {0};
+                    sprintf(nvs_key, "ade7953_ena_%c", ade7953->chip_num);
+                    nvs_set_i64(nvs_handle, nvs_key, (int64_t)(ade7953->data[0].activeEnergy * 1000));
+                    sprintf(nvs_key, "ade7953_enb_%c", ade7953->chip_num);
+                    nvs_set_i64(nvs_handle, nvs_key, (int64_t)(ade7953->data[1].activeEnergy * 1000));
+                    printf("saved energies to nvs!\n");
+                }
+
+                lastEnergySavedTime = xTaskGetTickCount();
+            }
+        }
+
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+}
---
+++ ade7953.h
@@ -0,0 +1,188 @@
+#ifndef __ADE7953__
+#define __ADE7953__
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+
+#define ADE7953_PREF              1540  // 4194304 / (1540 / 1000) = 2723574 (= WGAIN, VAGAIN and VARGAIN)
+#define ADE7953_UREF              26000 // 4194304 / (26000 / 10000) = 1613194 (= VGAIN)
+#define ADE7953_IREF              10000 // 4194304 / (10000 / 10000) = 4194303 (= IGAIN, needs to be different than 4194304 in order to use calib.dat)
+#define ADE7953_NO_LOAD_THRESHOLD 29196 // According to ADE7953 datasheet the default threshold for no load detection is 58,393 use half this value to measure lower (5w) powers.
+#define ADE7953_NO_LOAD_ENABLE    0     // Set DISNOLOAD register to 0 to enable No-load detection
+#define ADE7953_NO_LOAD_DISABLE   7     // Set DISNOLOAD register to 7 to disable No-load detection
+
+// Default calibration parameters can be overridden by a rule as documented above.
+#define ADE7953_GAIN_DEFAULT     0x400000 // = 0x400000 range 2097152 (min) to 6291456 (max)
+#define ADE7953_PHCAL_DEFAULT    0        // = range -383 to 383 - Default phase calibration for Shunts
+#define ADE7953_PHCAL_DEFAULT_CT 200      // = range -383 to 383 - Default phase calibration for Current Transformers (Shelly EM)
+
+#define ADE7953_MAX_CHANNEL 2
+
+#define ADE7953_CALIBREGS 6
+#define ADE7953_REGISTERS 6
+
+enum Ade7953_8BitRegisters
+{
+    // Register Name                    Addres  R/W  Bt  Ty  Default     Description
+    // ----------------------------     ------  ---  --  --  ----------  --------------------------------------------------------------------
+    ADE7953_SAGCYC = 0x000,      //      0x000   R/W  8   U   0x00        Sag line cycles
+    ADE7953_DISNOLOAD,           //      0x001   R/W  8   U   0x00        No-load detection disable (see Table 16)
+    ADE7953_RESERVED_0X002,      //      0x002
+    ADE7953_RESERVED_0X003,      //      0x003
+    ADE7953_LCYCMODE,            //      0x004   R/W  8   U   0x40        Line cycle accumulation mode configuration (see Table 17)
+    ADE7953_RESERVED_0X005,      //      0x005
+    ADE7953_RESERVED_0X006,      //      0x006
+    ADE7953_PGA_V,               //      0x007   R/W  8   U   0x00        Voltage channel gain configuration (Bits[2:0])
+    ADE7953_PGA_IA,              //      0x008   R/W  8   U   0x00        Current Channel A gain configuration (Bits[2:0])
+    ADE7953_PGA_IB,              //      0x009   R/W  8   U   0x00        Current Channel B gain configuration (Bits[2:0])
+    ADE7953_WRITE_PROT  = 0x040, //      0x040   R/W  8   U   0x00        Current Channel B gain configuration (Bits[2:0])
+    ADE7953_LAST_OP     = 0x0FD, //      0x0FD   R/W  8   U   0x00        Current Channel B gain configuration (Bits[2:0])
+    ADE7953_LAST_RWDATA = 0x0FF, //      0x0FF   R/W  8   U   0x00        Current Channel B gain configuration (Bits[2:0])
+    ADE7953_VERSION     = 0x702, //      0x702   R/W  8   U   0x00        Current Channel B gain configuration (Bits[2:0])
+    ADE7953_EX_REF      = 0x800, //      0x800   R/W  8   U   0x00        Current Channel B gain configuration (Bits[2:0])
+
+};
+
+enum Ade7953_16BitRegisters
+{
+    // Register Name                    Addres  R/W  Bt  Ty  Default     Description
+    // ----------------------------     ------  ---  --  --  ----------  --------------------------------------------------------------------
+    ADE7953_ZXTOUT = 0x100, //          0x100   R/W  16  U   0xFFFF      Zero-crossing timeout
+    ADE7953_LINECYC,        //          0x101   R/W  16  U   0x0000      Number of half line cycles for line cycle energy accumulation mode
+    ADE7953_CONFIG,         //          0x102   R/W  16  U   0x8004      Configuration register (see Table 18)
+    ADE7953_CF1DEN,         //          0x103   R/W  16  U   0x003F      CF1 frequency divider denominator. When modifying this register, two sequential write operations must be performed to ensure that the write is successful.
+    ADE7953_CF2DEN,         //          0x104   R/W  16  U   0x003F      CF2 frequency divider denominator. When modifying this register, two sequential write operations must be performed to ensure that the write is successful.
+    ADE7953_RESERVED_0X105, //          0x105
+    ADE7953_RESERVED_0X106, //          0x106
+    ADE7953_CFMODE,         //          0x107   R/W  16  U   0x0300      CF output selection (see Table 19)
+    ADE7943_PHCALA,         //          0x108   R/W  16  S   0x0000      Phase calibration register (Current Channel A). This register is in sign magnitude format.
+    ADE7943_PHCALB,         //          0x109   R/W  16  S   0x0000      Phase calibration register (Current Channel B). This register is in sign magnitude format.
+    ADE7943_PFA,            //          0x10A   R    16  S   0x0000      Power factor (Current Channel A)
+    ADE7943_PFB,            //          0x10B   R    16  S   0x0000      Power factor (Current Channel B)
+    ADE7943_ANGLE_A,        //          0x10C   R    16  S   0x0000      Angle between the voltage input and the Current Channel A input
+    ADE7943_ANGLE_B,        //          0x10D   R    16  S   0x0000      Angle between the voltage input and the Current Channel B input
+    ADE7943_Period,         //          0x10E   R    16  U   0x0000      Period register
+
+    ADE7953_RESERVED_0X120 =
+        0x120 // 0x120                            This register should be set to 30h to meet the performance specified in Table 1. To modify this register, it must be unlocked by setting Register Address 0xFE to 0xAD immediately prior.
+};
+
+enum Ade7953_32BitRegisters
+{
+    // Register Name                    Addres  R/W  Bt  Ty  Default     Description
+    // ----------------------------     ------  ---  --  --  ----------  --------------------------------------------------------------------
+    ADE7953_ACCMODE = 0x301, //         0x301   R/W  24  U   0x000000    Accumulation mode (see Table 21)
+
+    ADE7953_AP_NOLOAD = 0x303, //       0x303   R/W  24  U   0x00E419    No load threshold for actual power
+    ADE7953_VAR_NOLOAD,        //       0x304   R/W  24  U   0x00E419    No load threshold for reactive power
+    ADE7953_VA_NOLOAD,         //       0x305   R/W  24  U   0x000000    No load threshold for appearant power
+
+    ADE7953_AVA = 0x310,    //          0x310   R    24  S   0x000000    Instantaneous apparent power (Current Channel A)
+    ADE7953_BVA,            //          0x311   R    24  S   0x000000    Instantaneous apparent power (Current Channel B)
+    ADE7953_AWATT,          //          0x312   R    24  S   0x000000    Instantaneous active power (Current Channel A)
+    ADE7953_BWATT,          //          0x313   R    24  S   0x000000    Instantaneous active power (Current Channel B)
+    ADE7953_AVAR,           //          0x314   R    24  S   0x000000    Instantaneous reactive power (Current Channel A)
+    ADE7953_BVAR,           //          0x315   R    24  S   0x000000    Instantaneous reactive power (Current Channel B)
+    ADE7953_IA,             //          0x316   R    24  S   0x000000    Instantaneous current (Current Channel A)
+    ADE7953_IB,             //          0x317   R    24  S   0x000000    Instantaneous current (Current Channel B)
+    ADE7953_V,              //          0x318   R    24  S   0x000000    Instantaneous voltage (voltage channel)
+    ADE7953_RESERVED_0X319, //          0x319
+    ADE7953_IRMSA,          //          0x31A   R    24  U   0x000000    IRMS register (Current Channel A)
+    ADE7953_IRMSB,          //          0x31B   R    24  U   0x000000    IRMS register (Current Channel B)
+    ADE7953_VRMS,           //          0x31C   R    24  U   0x000000    VRMS register
+    ADE7953_RESERVED_0X31D, //          0x31D
+    ADE7953_AENERGYA,       //          0x31E   R    24  S   0x000000    Active energy (Current Channel A)
+    ADE7953_AENERGYB,       //          0x31F   R    24  S   0x000000    Active energy (Current Channel B)
+    ADE7953_RENERGYA,       //          0x320   R    24  S   0x000000    Reactive energy (Current Channel A)
+    ADE7953_RENERGYB,       //          0x321   R    24  S   0x000000    Reactive energy (Current Channel B)
+    ADE7953_APENERGYA,      //          0x322   R    24  S   0x000000    Apparent energy (Current Channel A)
+    ADE7953_APENERGYB,      //          0x323   R    24  S   0x000000    Apparent energy (Current Channel B)
+    ADE7953_OVLVL,          //          0x324   R/W  24  U   0xFFFFFF    Overvoltage level
+    ADE7953_OILVL,          //          0x325   R/W  24  U   0xFFFFFF    Overcurrent level
+    ADE7953_VPEAK,          //          0x326   R    24  U   0x000000    Voltage channel peak
+    ADE7953_RSTVPEAK,       //          0x327   R    24  U   0x000000    Read voltage peak with reset
+    ADE7953_IAPEAK,         //          0x328   R    24  U   0x000000    Current Channel A peak
+    ADE7953_RSTIAPEAK,      //          0x329   R    24  U   0x000000    Read Current Channel A peak with reset
+    ADE7953_IBPEAK,         //          0x32A   R    24  U   0x000000    Current Channel B peak
+    ADE7953_RSTIBPEAK,      //          0x32B   R    24  U   0x000000    Read Current Channel B peak with reset
+    ADE7953_IRQENA,         //          0x32C   R/W  24  U   0x100000    Interrupt enable (Current Channel A, see Table 22)
+    ADE7953_IRQSTATA,       //          0x32D   R    24  U   0x000000    Interrupt status (Current Channel A, see Table 23)
+    ADE7953_RSTIRQSTATA,    //          0x32E   R    24  U   0x000000    Reset interrupt status (Current Channel A)
+    ADE7953_IRQENB,         //          0x32F   R/W  24  U   0x000000    Interrupt enable (Current Channel B, see Table 24)
+    ADE7953_IRQSTATB,       //          0x330   R    24  U   0x000000    Interrupt status (Current Channel B, see Table 25)
+    ADE7953_RSTIRQSTATB,    //          0x331   R    24  U   0x000000    Reset interrupt status (Current Channel B)
+
+    ADE7953_CRC = 0x37F,    //          0x37F   R    32  U   0xFFFFFFFF  Checksum
+    ADE7953_AIGAIN,         //          0x380   R/W  24  U   0x400000    Current channel gain (Current Channel A)
+    ADE7953_AVGAIN,         //          0x381   R/W  24  U   0x400000    Voltage channel gain
+    ADE7953_AWGAIN,         //          0x382   R/W  24  U   0x400000    Active power gain (Current Channel A)
+    ADE7953_AVARGAIN,       //          0x383   R/W  24  U   0x400000    Reactive power gain (Current Channel A)
+    ADE7953_AVAGAIN,        //          0x384   R/W  24  U   0x400000    Apparent power gain (Current Channel A)
+    ADE7953_RESERVED_0X385, //          0x385
+    ADE7953_AIRMSOS,        //          0x386   R/W  24  S   0x000000    IRMS offset (Current Channel A)
+    ADE7953_RESERVED_0X387, //          0x387
+    ADE7953_VRMSOS,         //          0x388   R/W  24  S   0x000000    VRMS offset
+    ADE7953_AWATTOS,        //          0x389   R/W  24  S   0x000000    Active power offset correction (Current Channel A)
+    ADE7953_AVAROS,         //          0x38A   R/W  24  S   0x000000    Reactive power offset correction (Current Channel A)
+    ADE7953_AVAOS,          //          0x38B   R/W  24  S   0x000000    Apparent power offset correction (Current Channel A)
+    ADE7953_BIGAIN,         //          0x38C   R/W  24  U   0x400000    Current channel gain (Current Channel B)
+    ADE7953_BVGAIN,         //          0x38D   R/W  24  U   0x400000    This register should not be modified.
+    ADE7953_BWGAIN,         //          0x38E   R/W  24  U   0x400000    Active power gain (Current Channel B)
+    ADE7953_BVARGAIN,       //          0x38F   R/W  24  U   0x400000    Reactive power gain (Current Channel B)
+    ADE7953_BVAGAIN,        //          0x390   R/W  24  U   0x400000    Apparent power gain (Current Channel B)
+    ADE7953_RESERVED_0X391, //          0x391
+    ADE7953_BIRMSOS,        //          0x392   R/W  24  S   0x000000    IRMS offset (Current Channel B)
+    ADE7953_RESERVED_0X393, //          0x393
+    ADE7953_RESERVED_0X394, //          0x394
+    ADE7953_BWATTOS,        //          0x395   R/W  24  S   0x000000    Active power offset correction (Current Channel B)
+    ADE7953_BVAROS,         //          0x396   R/W  24  S   0x000000    Reactive power offset correction (Current Channel B)
+    ADE7953_BVAOS           //          0x397   R/W  24  S   0x000000    Apparent power offset correction (Current Channel B)
+};
+
+enum Ade7953CalibrationRegisters
+{
+    ADE7953_CAL_VGAIN,
+    ADE7953_CAL_IGAIN,
+    ADE7953_CAL_WGAIN,
+    ADE7953_CAL_VAGAIN,
+    ADE7953_CAL_VARGAIN,
+    ADE7943_CAL_PHCAL
+};
+
+typedef enum
+{
+    ADE7953_SHELLY_25,
+    ADE7953_SHELLY_EM,
+    ADE7953_SHELLY_PLUS_2PM,
+    ADE7953_SHELLY_PRO_1PM,
+    ADE7953_SHELLY_PRO_2PM,
+    ADE7953_SHELLY_PRO_4PM
+} ShellyModels_t;
+
+typedef struct {
+    float  voltage;
+    float  current;
+    float  activePower;
+    double activeEnergy;
+} ade7953_data_t;
+
+typedef struct {
+    int            i2c_port_number;
+    uint8_t        addr;
+    int32_t        calib_data[ADE7953_MAX_CHANNEL][ADE7953_CALIBREGS];
+    char           chip_num;
+    ade7953_data_t data[ADE7953_MAX_CHANNEL];
+    bool           saveEnergiesToNvs;
+} __attribute__((packed)) ade7953_t;
+
+void    ade7953_init(ade7953_t* ade7953, ShellyModels_t model);
+int32_t ade7953_readReg(ade7953_t* ade7953, uint16_t reg, TickType_t ticks_to_wait);
+void    ade7953_writeReg(ade7953_t* ade7953, uint16_t reg, int32_t data, TickType_t ticks_to_wait);
+void    ade7953_setDefaultsForShellyDevice(ade7953_t* ade7953, ShellyModels_t model);
+void    ade7953_setCalibrationForShellyDevice(ade7953_t* ade7953, ShellyModels_t model);
+void    ade7953_retrieveCalibrationFromNvs(ade7953_t* ade7953);
+void    ade7953_setCalibrationForShellyDevice(ade7953_t* ade7953, ShellyModels_t model);
+
+#endif /* __ADE7953__ */
